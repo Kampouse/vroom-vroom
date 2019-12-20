85b286133a382ea299b6510d816ac1a073ac8453
Kampouse <jpmartel98@gmail.com>
2019-12-20 22:14:43 +0000

    everythjing

--- main.lua
+++ main.lua
@@ -1,2 +1,10 @@
-print("yes")
-a* 
+local sprite
+local x 
+local y 
+local virtualwidth = 432
+local virtualheight = 243
+local speeed = 10
+
+function love.load()
+love.graphics.setDefaultFilter('nearest','nearest')
+sprite = love.graphics.newImage('graphics/')
