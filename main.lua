b0097499526375041a45c759a75c617d20e7ad5b
Kampouse <jpmartel98@gmail.com>
2019-12-20 23:51:02 +0000

    everthingelse

--- main.lua
+++ main.lua
@@ -1,10 +1,37 @@
-local sprite
+  local sprite
 local x 
 local y 
-local virtualwidth = 432
-local virtualheight = 243
+local virtualWidth = 432
+local virtualHeight = 243
 local speeed = 10
 
 function love.load()
-love.graphics.setDefaultFilter('nearest','nearest')
-sprite = love.graphics.newImage('graphics/')
+    love.graphics.setDefaultFilter('nearest','nearest')
+
+    sprite = love.graphics.newImage('graphics/test.png')
+    x = virtualWidth / 2 - sprite: getWidth() / 2
+    y = virtualHeight / 2 - sprite: getHeight() / 2
+
+    
+end
+function love.update(dt)
+end
+
+function love.keypressed(key)
+if key == 'a' then
+    x = x + 10
+end
+
+if key == right 'b' then 
+    x = x - 10 
+
+end
+if key == 'escap' then 
+    love.event.quit()
+    end
+end
+
+function love.draw()
+    print(x)
+    love.graphics.draw(sprite,x,y)
+end
